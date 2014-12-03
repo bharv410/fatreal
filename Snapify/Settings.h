7590f5963df997d77e9c1bb8723911d3900b2563
Panafold <panafold@kit-halvorsens-imac-2.local>
2014-12-02 16:00:19 -0800

    First commit

---
+++ Snapify/Settings.h
@@ -0,0 +1,29 @@
+//
+//  Settings.h
+//  Snapify
+//
+//  Created by Christopher Coudriet on 12/31/13.
+//  Copyright Christopher Coudriet 2013. All rights reserved.
+//
+//  Permission is given to license this source code file, in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that this source code cannot be redistributed or sold (in part or whole) and must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
+//
+
+#pragma mark - App Settings
+
+// ParseAppId
+ #define ParseAppId      @"rW19JzkDkzkgH5ZuqDO9wgD43XIfqEdnznw8YftG"
+
+// ParseClientKey
+ #define ParseClientKey  @"sxRJveZXQvLlvlfWzf0949RFTyvIaJOvJeC1WtoI"
+
+#ifndef ParseAppId
+#error "Must define ParseAppId. Please visit http://www.parse.com to signup"
+#endif
+
+#ifndef ParseClientKey
+#error "Must define ParseClientKey. Please visit http://www.parse.com to signup"
+#endif
